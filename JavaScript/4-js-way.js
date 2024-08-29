27c9cac6493202073085b54fa0fb78b156609ca6
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-08-29 20:12:07 +0000

    Add strategy/module solution

---
+++ JavaScript/4-js-way.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const dataAccessStrategies = {
+  fs: require('./4-fs.js'),
+  pg: require('./4-pg.js'),
+  minio: require('./4-minio.js'),
+};
+
+// Usage
+
+(async () => {
+  const dal = dataAccessStrategies['fs'];
+  const db = dal.createDatabase('./storage.dat');
+  const cursor = db.select({ city: 'Roma' });
+  for await (const record of cursor) {
+    console.dir(record);
+  }
+})();
